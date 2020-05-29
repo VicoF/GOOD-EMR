9104f1e9f579be2e52ec157e42b145c5964720ee
Victor <vicfreve@gmail.com>
2020-05-29 13:36:05 -0400

    coucouo

---
+++ src/main/java/application/EMR.java
@@ -0,0 +1,16 @@
+package main.java.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class EMR Appl extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/main/java/application/EMRApplication.java
@@ -0,0 +1,9 @@
+package main.java.application;
+
+public class EMRApplication {
+
+
+    public static void main(String[] args) {
+
+    }
+}
