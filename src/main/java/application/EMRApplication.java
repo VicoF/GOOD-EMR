422e45bfc765a6cc0b33e0c731ca288fb1da6938
Victor <vicfreve@gmail.com>
2020-05-29 13:46:19 -0400

    build.gradle

--- src/main/java/application/EMRApplication.java
+++ src/main/java/application/EMRApplication.java
@@ -1,9 +1,19 @@
 package main.java.application;
 
-public class EMRApplication {
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+
+
+public class EMRApplication extends Application {
 
 
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
 
     }
 }
