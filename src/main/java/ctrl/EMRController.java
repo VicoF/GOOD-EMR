92fabcd35f1e59c87a171f3f9877fc48f13d2103
Victor <vicfreve@gmail.com>
2020-05-31 21:47:59 -0400

    Prise en charge bouton toolbar + canva resizable

--- src/main/java/ctrl/EMRController.java
+++ src/main/java/ctrl/EMRController.java
@@ -1,32 +1,34 @@
 package ctrl;
 
-import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Bounds;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Accordion;
-import javafx.scene.control.TitledPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.*;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.*;
 
-import javafx.scene.paint.Color;
+
+import models.EMRCanvas;
 import models.composants.EMRCategories;
 import models.composants.EMRShape;
 import models.composants.EMRShapeFactory;
 import models.composants.InversionAccumulationShape;
 import models.composants.InversionConversionShape;
 import models.composants.InversionCouplingShape;
-
-
-import java.util.ArrayList;
+import models.modes.DragMode;
+import models.modes.DrawMode;
+import models.modes.EraseMode;
+import models.modes.Mode;
 
 
 public class EMRController {
 
     @FXML
-    Canvas canva;
+    EMRCanvas canva;
+    @FXML
+    Pane canvaParent;
 
     @FXML
     Accordion accordion;
@@ -52,19 +54,32 @@ public class EMRController {
     @FXML
     BorderPane pane;
 
-    ArrayList<EMRShape> shapes;
+    @FXML
+    Button drawButton;
+
+    @FXML
+    Button eraseButton;
+
+    @FXML
+    Button dragButton;
+
+    @FXML
+    Label modeLabel;
+
+
 
     EMRShape draggedShape = null;
 
-    public EMRController() {
-        shapes = new ArrayList<>();
-    }
+    Mode mode = new DrawMode();
+
+
 
 
     public void initialize() {
-        GraphicsContext gc = canva.getGraphicsContext2D();
-        gc.setFill(Color.WHITE);
-        gc.fillRect(0, 0, canva.getWidth(), canva.getHeight());
+       canva.widthProperty().bind(canvaParent.widthProperty());
+       canva.heightProperty().bind(canvaParent.heightProperty());
+
+
         Canvas currentCanva = inversionAccumulationCanva;
         EMRShape s = EMRShapeFactory.getComposant(EMRShapeFactory.EMRShapeType.INVERSION_ACCUMULATION,EMRCategories.INVERSION_BASED, (int) (currentCanva.getWidth()/2),(int) (currentCanva.getHeight()/2));
         s.draw(currentCanva.getGraphicsContext2D());
@@ -79,15 +94,14 @@ public class EMRController {
         canva.setOnDragOver(event -> {
             /* data is dragged over the target */
             System.out.println("onDragOver");
-            // On retire la shape que l'on drag de la liste
-            shapes.remove(draggedShape);
+            // On retire la shape que l'on drag du canva
+            canva.eraseShape(draggedShape);
             //On met à jour ses données
             draggedShape.setPosX(event.getX());
             draggedShape.setPosY(event.getY());
-            //On la remet dans la liste
-            shapes.add(draggedShape);
-            // On redessine
-           redrawCanva();
+            //On la remet dans le canva
+            canva.drawShape(draggedShape);
+
 
             event.consume();
         });
@@ -106,6 +120,7 @@ public class EMRController {
         ClipboardContent content = new ClipboardContent();
         content.putString("Allo");
         db.setContent(content);
+
        if(event.getSource().equals(inversionCouplingCanva)) {
            draggedShape = EMRShapeFactory.getComposant(EMRShapeFactory.EMRShapeType.INVERSION_COUPLING, EMRCategories.INVERSION_BASED, 0, 0);
        }else if (event.getSource().equals(inversionAccumulationCanva)){
@@ -117,52 +132,38 @@ public class EMRController {
         event.consume();
     }
 
+
     @FXML
-    public void onTitledPaneClicked(MouseEvent event) {
-        System.out.println("allooo");
-        TitledPane expandedPane = accordion.getExpandedPane();
-
-        if (expandedPane != null) {
-            FlowPane currentPane = (FlowPane) expandedPane.getContent();
-           energyBasedPane.getChildren().add(EMRShapeFactory.getComposant(EMRShapeFactory.EMRShapeType.INVERSION_ACCUMULATION,EMRCategories.INVERSION_BASED,0,0).getShape());
-            }
+    public void onToolBarButtonClicked(ActionEvent event){
+        modeLabel.setText("Bouton cliqué");
+        if(event.getSource().equals(drawButton)){
+            mode = new DrawMode();
+            modeLabel.setText("Outil de dessin selectionne, glissez sur le canva pour dessiner une fleche");
+        }else if(event.getSource().equals(eraseButton)){
+            mode = new EraseMode();
+            modeLabel.setText("Outil efface selectionne, appuyez sur une forme pour l'effacer");
+        }else if(event.getSource().equals(dragButton)){
+            mode = new DragMode();
+            modeLabel.setText("Outil de glissement selectionne, glissez sur le canva pour dessiner deplacer les formes");
         }
-
-
+    }
 
     @FXML
     public void onCanvaClicked(MouseEvent event) {
         switch (event.getButton()) {
             case PRIMARY:
-                shapes.add(new InversionCouplingShape(EMRCategories.INVERSION_BASED, event.getX(), event.getY()));
+                canva.drawShape(new InversionCouplingShape(EMRCategories.INVERSION_BASED, event.getX(), event.getY()));
                 break;
             case SECONDARY:
-                shapes.add(new InversionAccumulationShape(EMRCategories.INVERSION_BASED, event.getX(), event.getY()));
+                canva.drawShape(new InversionAccumulationShape(EMRCategories.INVERSION_BASED, event.getX(), event.getY()));
                 break;
             case MIDDLE:
-                shapes.add(new InversionConversionShape(EMRCategories.INVERSION_BASED, event.getX(), event.getY()));
+                canva.drawShape(new InversionConversionShape(EMRCategories.INVERSION_BASED, event.getX(), event.getY()));
                 break;
         }
-        GraphicsContext gc = canva.getGraphicsContext2D();
-        gc.setFill(Color.BLACK);
-        gc.fillText("Forme", 0, 0);
-        for (EMRShape s : shapes) {
-
-            s.draw(gc);
-
-        }
 
     }
 
-    /**
-     * Permet de redessiner le canva à l'aide de la liste de forme du controleur
-     */
-    private void redrawCanva(){
-    GraphicsContext gc = canva.getGraphicsContext2D();
-    gc.setFill(Color.WHITE);
-    gc.fillRect(0, 0, canva.getWidth(), canva.getHeight());
-    for(EMRShape shape:shapes){
-        shape.draw(gc);
-    }
-}
+
+
 }
