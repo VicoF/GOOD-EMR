f4f65976665d75c5cf9f57d390da8795838e0bb2
Victor <vicfreve@gmail.com>
2020-06-02 14:05:33 -0400

    Window size

--- src/main/java/application/EMRApplication.java
+++ src/main/java/application/EMRApplication.java
@@ -1,8 +1,10 @@
 package application;
 
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.application.Application;
 
@@ -13,9 +15,11 @@ public class EMRApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/views/windproto1.fxml"));
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        primaryStage.setTitle("EMR APP");
 
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
